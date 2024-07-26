5ed27432280725a41a167e68796cd9d71a6f70e7
imyou69 <121504604+imyou69@users.noreply.github.com>
2024-07-26 08:40:04 +0530

    binary_search.c

--- B_tree.c
+++ B_tree.c
@@ -1,81 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-struct st {
-    int data;
-    struct st *left;
-    struct st *right;
-};
-typedef struct st* NODE;
+int BinarySearch(int array[], int start_index, int end_index, int element) {
+    if (end_index >= start_index) {
+        int middle = start_index + (end_index - start_index) / 2;
+        if (array[middle] == element)
+            return middle;
+        if (array[middle] > element)
+            return BinarySearch(array, start_index, middle - 1, element);
+        return BinarySearch(array, middle + 1, end_index, element);
+    }
+    return -1;
+}
 
-struct st *root = NULL, *temp;
-
-void insert() {
-    struct st *nc, *pNode;
-    int v;
-    printf("Enter the value of the node\n");
-    scanf("%d", &v);
-    temp = (NODE)malloc(sizeof(struct st));
-    temp->data = v;
-    temp->left = NULL;
-    temp->right = NULL;
-    if (root == NULL) {
-        root = temp;
+int main(void) {
+    int array[20], n, element, i = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements in sorted order: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+    printf("Enter key value: ");
+    scanf("%d", &element);
+    int found = BinarySearch(array, 0, n - 1, element);
+    if (found == -1) {
+        printf("Element not found in the array\n");
     } else {
-        nc = root;
-        while (nc != NULL) {
-            pNode = nc;
-            if (v < nc->data)
-                nc = nc->left;
-            else
-                nc = nc->right;
-        }
-        if (v < pNode->data) {
-            pNode->left = temp;
-        } else {
-            pNode->right = temp;
-        }
-    }
-}
-
-void preorder(struct st *temp) {
-    if (temp != NULL) {
-        printf("%d ", temp->data);
-        preorder(temp->left);
-        preorder(temp->right);
-    }
-}
-
-void inorder(struct st *temp) {
-    if (temp != NULL) {
-        inorder(temp->left);
-        printf("%d ", temp->data);
-        inorder(temp->right);
-    }
-}
-
-void postorder(struct st *temp) {
-    if (temp != NULL) {
-        postorder(temp->left);
-        postorder(temp->right);
-        printf("%d ", temp->data);
-    }
-}
-
-int main() {
-    int ch;
-    while (1) {
-        printf("\n 1.insert Elements \n 2.preorder \n 3.inorder \n 4.postorder \n 5.exit");
-        printf("\n enter your choice ");
-        scanf("%d", &ch);
-        switch (ch) {
-            case 1: insert(); break;
-            case 2: preorder(root); break;
-            case 3: inorder(root); break;
-            case 4: postorder(root); break;
-            case 5: exit(0);
-            default: printf("Invalid operation");
-        }
+        printf("Element found at index: %d\n", found + 1); // Printing as one-based index
     }
     return 0;
 }
